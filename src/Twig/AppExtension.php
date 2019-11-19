de92d91027f2b73d589f9583aa65a13b81cdea0b
Dan Thorpe <dant@linx.net>
2019-11-19 11:06:23 +0000

    Add Slugify, add custom twig filter to replace front-end clan names

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class AppExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('clan_remove', [$this, 'clanRemove']),
+        ];
+    }
+
+    public function clanRemove($subject)
+    {
+        return trim(preg_replace('/\[.*?\]/is', '', $subject));
+    }
+}
