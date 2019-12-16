71704f0878383f4c385f8741ff1c0328dd20032f
Dan Thorpe <dant@linx.net>
2019-12-16 12:07:30 +0000

    Use batching for the get player clan IDs command to prevent php memory issues

--- src/Command/GetPlayerClanIdsCommand.php
+++ src/Command/GetPlayerClanIdsCommand.php
@@ -6,6 +6,7 @@ use App\Entity\Clan;
 use App\Entity\Player;
 use App\Service\Smite;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -41,51 +42,69 @@ class GetPlayerClanIdsCommand extends Command
         $clanRepo = $this->entityManager->getRepository(Clan::class);
         $playerRepo = $this->entityManager->getRepository(Player::class);
 
-        $players = $playerRepo->findByTeamIdNotNull();
-        $clanIds = [];
-        if (!empty($players)) {
+        $playersCount = $playerRepo->count([]);
+        $perPage = '500';
+        $pageCount = ceil($playersCount / $perPage);
+
+        $output->writeln("{$playersCount} players to grab clan IDs from.");
+        $output->writeln("Players per batch {$perPage}");
+
+        $query = $playerRepo->findNewestPlayerNameNotNullQuery();
+        $paginator = new Paginator($query);
+
+        $i = 0;
+        $totalStoredClansCount = 0;
+
+        for ($p=1; $p<=$pageCount; $p++) {
+            $output->writeln("Player batch {$p} of {$pageCount}...");
+
+            $offset = $perPage * ($p-1);
+            $paginator
+                ->getQuery()
+                ->setFirstResult($offset)
+                ->setMaxResults($perPage);
+
+            $clanIds = [];
             /** @var Player $player */
-            foreach ($players as $player) {
+            foreach ($paginator as $player) {
                 $clan = $clanRepo->findOneBy(['smiteClanId' => $player->getTeamId()]);
-                if (is_null($clan) && !in_array($player->getTeamId(), $clanIds)) {
+                if (is_null($clan) && !in_array($player->getTeamId(), $clanIds) && !is_null($player->getTeamId())) {
                     $clanIds[] = $player->getTeamId();
                 }
+                $i++;
             }
-        }
+            $newClanIdsCount = count($clanIds);
 
-        $storedClansCount = 0;
-        if (!empty($clanIds)) {
-            foreach ($clanIds as $clanId) {
-                $newClan = new Clan();
-                $newClan->setSmiteClanId($clanId);
-                $newClan->setDateCreated(new \DateTime());
-                $newClan->setDateUpdated(new \DateTime());
-                $this->entityManager->persist($newClan);
-                $storedClansCount++;
+            $storedClansCount = 0;
+            if (!empty($clanIds)) {
+                $output->writeln("{$newClanIdsCount} new clan IDs!");
 
-                $clanDetails = $this->smite->getTeamDetails($clanId);
-                if (!empty($clanDetails) && $clanDetails['TeamId'] !== 0) {
-                    $newClan->setName($clanDetails['Name']);
-                    $newClan->setFounder($clanDetails['Founder']);
-                    $newClan->setTag($clanDetails['Tag']);
-                    $newClan->setPlayers($clanDetails['Players']);
-                    $newClan->setWins($clanDetails['Wins']);
-                    $newClan->setLosses($clanDetails['Losses']);
-                    $newClan->setRating($clanDetails['Rating']);
-                    $newClan->setCrawled(1);
+                foreach ($clanIds as $clanId) {
+                    $newClan = new Clan();
+                    $newClan->setSmiteClanId($clanId);
+                    $newClan->setDateCreated(new \DateTime());
+                    $newClan->setDateUpdated(new \DateTime());
                     $this->entityManager->persist($newClan);
+                    $storedClansCount++;
+                    $totalStoredClansCount++;
+
+                    $clanDetails = $this->smite->getTeamDetails($clanId);
+                    if (!empty($clanDetails) && $clanDetails['TeamId'] !== 0) {
+                        $newClan->setName($clanDetails['Name']);
+                        $newClan->setFounder($clanDetails['Founder']);
+                        $newClan->setTag($clanDetails['Tag']);
+                        $newClan->setPlayers($clanDetails['Players']);
+                        $newClan->setWins($clanDetails['Wins']);
+                        $newClan->setLosses($clanDetails['Losses']);
+                        $newClan->setRating($clanDetails['Rating']);
+                        $newClan->setCrawled(1);
+                        $this->entityManager->persist($newClan);
+                    }
                 }
+                $this->entityManager->flush();
+                $output->writeln("{$storedClansCount} new clans added!");
             }
-            $this->entityManager->flush();
         }
-
-        $playersCount = count($players);
-        $newClanIdsCount = count($clanIds);
-
-        $output->writeln("{$playersCount} players to grab clan IDs from...");
-        $output->writeln("{$newClanIdsCount} new clan IDs!");
-        $output->writeln("{$storedClansCount} new clans added!");
-
         return 0;
     }
 }
