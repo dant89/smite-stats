b9274bc1007b441d298017b51fbe053138d0a9fc
Dan Thorpe <dant@linx.net>
2020-02-04 21:28:02 +0000

    Fix null match mapper

--- src/Mapper/MatchPlayerAbilityMapper.php
+++ src/Mapper/MatchPlayerAbilityMapper.php
@@ -9,15 +9,15 @@ class MatchPlayerAbilityMapper
 {
     public function from(array $data, int $banCount, MatchPlayer $matchPlayer): ?MatchPlayerAbility
     {
-        if (is_null($data["ItemId{$banCount}"]) && is_null($data["Item_Purch_{$banCount}"])) {
-            return null;
-        }
-
         $matchPlayerAbility = new MatchPlayerAbility();
         $matchPlayerAbility->setAbilityId($data["ItemId{$banCount}"] ?: null);
         $matchPlayerAbility->setAbilityName($data["Item_Purch_{$banCount}"] ?: null);
         $matchPlayerAbility->setAbilityNumber($banCount);
         $matchPlayerAbility->setMatchPlayer($matchPlayer);
+
+        if (is_null($matchPlayerAbility->getAbilityId()) && is_null($matchPlayerAbility->getAbilityName())) {
+            return null;
+        }
 
         return $matchPlayerAbility;
     }
